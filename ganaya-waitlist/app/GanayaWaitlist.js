fb3d3db2732b6908fdc5b22e6657db80a35c9692
Ganaya-app <ganayaapp2026@gmail.com>
2026-04-03 13:50:49 -0300

    Update GanayaWaitlist.js
    Se agregó pixel de meta

--- ganaya-waitlist/app/GanayaWaitlist.js
+++ ganaya-waitlist/app/GanayaWaitlist.js
@@ -80,6 +80,15 @@ export default function GanayaWaitlist() {
     const ref = new URLSearchParams(window.location.search).get("ref");
     if (ref) setReferredBy(ref);
     getCount().then(n => { if (n > 0) setTotalSignups(n); }).catch(() => {});
+
+    // Meta Pixel
+    const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+    if (pixelId && typeof window !== "undefined" && !window.fbq) {
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+      window.fbq('init', pixelId);
+      window.fbq('track', 'PageView');
+    }
+
     track("page_view");
   }, []);
 
